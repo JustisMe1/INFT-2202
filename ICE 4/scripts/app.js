6058b3bb589194669b1bffee4f773f2acbb86f9f
JustisMe1 <156385515+JustisMe1@users.noreply.github.com>
2024-02-09 17:35:05 -0500

    Final script COMMIT
    Finalized version of app.js script

--- ICE 4/scripts/app.js
+++ ICE 4/scripts/app.js
@@ -1,13 +1,17 @@
 console.log('app.js loaded')
 // TO DO: load the user class script with alias
+//Imports
+import * as userClass from "./user.js";
+import * as formValidation from "./form.js";
 
-
+let newUser = new userClass.User("Justis", "McGill", "justisme", "jdoe@dcmail.ca", "user1");
+console.log(newUser.displayUser());
 
 /**
  * iffe to insert nav bar at the top of each page
  */
 $(function () {
-    let navBar = `<div class="naviation">
+    let navBar = `<div class="navigation">
 	                <nav class="nav">
 		                <a class="nav-link active" href="index.html">Home</a>
 		                <a class="nav-link" href="slideshow.html">Slideshow</a>
@@ -17,7 +21,7 @@ $(function () {
                 </div>`
 
     //TO DO: replace the nav-holder
-
+    $("#nav-holder").replaceWith(navBar);
 });
 
 /**
@@ -31,146 +35,179 @@ $(function () {
 /**
  * fuction to demo adding content with .text() and .html()
  *  */
-// $(function () {
+$(function () {
 
-    // let navDiv = $("div:first");
-    // console.log(navDiv.html())
-    // console.log(navDiv.text())
+    let navDiv = $("div:first");
+    //console.log(navDiv.html())
+    //console.log(navDiv.text())
 
-    // let contentDiv = $("#content-div");
-    // console.log(contentDiv.html())
-    // console.log(contentDiv.text())
+    let contentDiv = $("#content-div");
+    //console.log(contentDiv.html())
+    //console.log(contentDiv.text())
 
-    // text to use with new paragraph
-    // text = "It is one of his best works!"
+    //text to use with new paragraph
+    let text = "It is one of his best works!"
     // create a paragraph
-    // let newP = $("<p></p>");
+    let newP = $("<p></p>");
     // TO DO: add text with html()
-
-    // append to contentDiv
+    newP.text(text);
+    //append to contentDiv
+    contentDiv.append(newP);
 
     // add text
-    // newP.text("I am very excited for the opening of the new adaptation of his work.");
+    newP.text("I am very excited for the opening of the new adaptation of his work.");
     // TO DO: test when very excited is in a strong tag
-
+    newP.html("I am <strong>very excited</strong> for the opening of the new adaptation of his work.");
     // TO DO: append to newP text
 
-// });
+});
 
 /**
  * function to demo adding toggle to button
  *  */
-// $(function () {
-    // TO DO: complete the function
-    // get the button
-    // add a click function
-        // get the parent div's p tags
-        // for each p in the div
-            // if it has toggleHide class
-                //remove toggleHide class and add toggleShow class
-                // styling is controlled in the css
-            // otherwise assume it has the toggleShow class
-                //remove toggleShow and add toggleHide
+$(function () {
+    //TO DO: complete the function
+    //get the button
+    let btnHide = $("#toggleDivBtn");
+    //add a click function
+    btnHide.click(function(){
+        //get the parent div's p tags
+        let para = $("#content-div").find("p");
+        
+        //for each p in the div
+        $(para).each(function(){
 
-//  });
+            //if it has toggleHide class
+            if ($(this).attr("class") == "toggleHide")
+            {
+                //remove toggleHide class and add toggleShow class
+                $(this).attr("class", "toggleShow");
+                //styling is controlled in the css
+                $(this).show();
+            } else
+            {
+                $(this).attr("class", "toggleHide");
+                //otherwise assume it has the toggleShow class
+                $(this).hide();
+                //remove toggleShow and add toggleHide
+            }
+        });
+    });
+});
 
 
 
 // FORM JQUERY
 // TO DO: import form validation functions with alias
+//Done at top of page
 
 // if the submit button is on the page
-// if ($("#btnRegSubmit")) {
+if ($("#btnRegSubmit")) {
     // TO DO: add a click function that calls a callack function
-    // $("#btnRegSubmit").click(function (e) {
+    $("#btnRegSubmit").click(function (e) {
         // prevent the default submit action (stay on the page)
+        e.preventDefault();
 
         // create a new user
         // you normally wouldn't do this unless you had validated, but we're going to do it to show how class memebers work in calling the validation
-
+        const unvalidUser = new userClass.User(
             // get the first name input
-
+            $("#inputFirst").val(),
             // get the last name input
-
+            $("#inputLast").val(),
             // get the username input
-
+            $("#inputUsername").val(),
             // get the email input
-
+            $("#inputEmail").val(),
             // get the password input
-
+            $("#inputPassword").val()
+        );
+            
 
 
         // debug statement for object
         // console.log(`UserDetails: ${user.displayUser()}`)
 
         // validate first name
-
+        $("#first-group").children(".errorMessage").html(formValidation.validateFirst(unvalidUser.firstName));
         // validate last name
-
+        $("#last-group").children(".errorMessage").html(formValidation.validateLast(unvalidUser.firstName));
         // validate  username
-
+        $("#username-group").children(".errorMessage").html(formValidation.validateUsername(unvalidUser.username));
         // validate confirm password
-
-// });
-// }
+        let error = formValidation.validatePassword($("inputPassword").val(), $("inputPassword2").val());
+        $("#pass1-group").children(".errorMessage").html(error);
+        $("#pass2-group").children(".errorMessage").html(error);
+    });
+}
 
 // TO DO: if reset button present
-
+if($("#btnRegReset"))
+{
     // bind a click event handler
-
+    $("#btnRegReset").click(function (e){
         // clear out all error message paragraphs
+        $("#errorMessage").html("<p></p>");
+    });
+}
 
 
 
 
 // SLIDESHOW
 // TO DO: if there's a gallery class on the page
-
-    // call a callback function to handle the galler rotation
-
-
+if($(".gallery"))
+{
+    // call a callback function to handle the gallery rotation
+    $(function(){
         // get the image tag
+        let galleryImage = $(".gallery").find("img").first();
         // get a list of your images
-        // let images = [
-        //     "./images/portraits/portrait-01.jpg",
-        //     "./images/portraits/portrait-02.jpg",
-        //     "./images/portraits/portrait-03.jpg",
-        //     "./images/portraits/portrait-04.jpg",
-        //     "./images/portraits/portrait-05.jpg",
-        //     "./images/portraits/portrait-06.jpg",
-        //     "./images/portraits/portrait-07.jpg",
-        //     "./images/portraits/portrait-08.jpg",
-        //     "./images/portraits/portrait-09.jpg",
-        //     "./images/portraits/portrait-10.jpg",
-        //     "./images/portraits/portrait-11.jpg",
-        //     "./images/portraits/portrait-12.jpg",
-        //     "./images/portraits/portrait-13.jpg",
-        //     "./images/portraits/portrait-14.jpg",
-        //     "./images/portraits/portrait-15.jpg",
-        //     "./images/portraits/portrait-16.jpg",
-        //     "./images/portraits/portrait-17.jpg",
-        //     "./images/portraits/portrait-18.jpg",
-
-        // ]
-
-
+        let images = [
+            "./images/portraits/portrait-01.jpg",
+            "./images/portraits/portrait-02.jpg",
+            "./images/portraits/portrait-03.jpg",
+            "./images/portraits/portrait-04.jpg",
+            "./images/portraits/portrait-05.jpg",
+            "./images/portraits/portrait-06.jpg",
+            "./images/portraits/portrait-07.jpg",
+            "./images/portraits/portrait-08.jpg",
+            "./images/portraits/portrait-09.jpg",
+            "./images/portraits/portrait-10.jpg",
+            "./images/portraits/portrait-11.jpg",
+            "./images/portraits/portrait-12.jpg",
+            "./images/portraits/portrait-13.jpg",
+            "./images/portraits/portrait-14.jpg",
+            "./images/portraits/portrait-15.jpg",
+            "./images/portraits/portrait-16.jpg",
+            "./images/portraits/portrait-17.jpg",
+            "./images/portraits/portrait-18.jpg",
+        ]
 
         // set a first index
+        let imageIndex = 0;
 
         // call the setInterval method that will re-call this method at a set interval
-
+        setInterval(function(){
             // increment the image index but no greater than how many images you have
+            imageIndex++ % images.length;
 
             // fade out the current image
-
+            galleryImage.fadeOut(1000, function(){
                 // $(this) refers to the object that calls the callback or in this case galleryImage
                 // change the src attribute of the image
+                $(this).attr("src", images[imageIndex]);
 
                 // fade it back in
+                $(this).fadeIn(1000);
 
                 // debug statement
-
+                console.log($(this).attr("src"));
+            });
             //set the time for more than how long the fade out and in process will take or you won't get the images you expect
+        }, 3000);
+    })
+}
 
 
 
